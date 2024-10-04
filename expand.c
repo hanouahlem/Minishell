a0599ee47af193b0d048d0be496c57b1cb295b28
Ahlem Bey <ahbey@paul-f4Br7s6.clusters.42paris.fr>
2024-10-04 15:39:00 +0200

    merge with manar

--- expand.c
+++ expand.c
@@ -6,7 +6,7 @@
 /*   By: ahbey <ahbey@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 17:48:45 by manbengh          #+#    #+#             */
-/*   Updated: 2024/10/04 14:32:34 by ahbey            ###   ########.fr       */
+/*   Updated: 2024/10/04 15:35:37 by ahbey            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,51 +23,49 @@ char	*ft_value_from_key(char *str, t_mini *data)
 		{
 			printf("%s = %s\n", str, tmp->value);
 			return (tmp->value);
-			
-		}	
+		}
 		tmp = tmp->next;
 	}
 	return (NULL);
 }
 
-char    *ft_get_key(char *str, t_mini *data)
+char	*ft_get_key(char *str, t_mini *data)
 {
-        int     i;
-	int	j;
+	int		i;
+	int		j;
 	char	*c;
 
-        i = 0;
+	i = 0;
 	j = 0;
 	c = malloc(sizeof(char) * ft_strlen(str) + 1);
-        if (str[i] == '$')
-                i++;
-        while (str[i] && (ft_isalnum(str[i]) || str[i] == '_') && !ft_isdigit(str[1]))
+	if (str[i] == '$')
+		i++;
+	while (str[i] && (ft_isalnum(str[i]) || str[i] == '_')
+		&& !ft_isdigit(str[1]))
 		c[j++] = str[i++];
 	c[j] = '\0';
 	return (ft_value_from_key(c, data));
 }
 
-
 // $USER"lala"$USER
-int join_after_key()
-{	
-	char *str;
-	char *res = NULL;
-	int i = 0;
-	while(str[i])
-	{
-		if(str[i] == '$')
-		{
-			res = ft_strjoin(res, ft_get_key());
-			res = "manbenghlalamanbengh";
-			while(str[i] = ft_isalnum)
-				i++;
-		}
-		else
-		{
-			res = ft_strcat(str[i]);
-			// "manbenghlala"
-		}
-		i++;
-	}
-}
+// int	join_after_key(char *str, t_mini *data)
+// {
+// 	char *res = NULL;
+// 	int i = 0;
+// 	while (str[i])
+// 	{
+// 		if (str[i] == '$')
+// 		{
+// 			res = ft_strjoin(res, ft_get_key(str, data));
+// 			res = "manbenghlalamanbengh";
+// 			while (str[i] = ft_isalnum)
+// 				i++;
+// 		}
+// 		else
+// 		{
+// 			res = ft_strcat(str[i]);
+// 			// "manbenghlala"
+// 		}
+// 		i++;
+// 	}
+// }
