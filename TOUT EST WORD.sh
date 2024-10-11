99747ffa63dda7c5c605d0c57873759801c2b0de
manbengh <bengharbi.manar@gmail.com>
2024-10-11 20:41:25 +0200

    bon token dans data **token

---
+++ TOUT EST WORD.sh
@@ -0,0 +1,36 @@
+TOUT EST WORD
+sauf REDIR et ce qui suit une REDIR est un FICHIER
+
+< blabla < -R > ls < avion bateau echo > -c < -e cat
+cmd = bateau
+arg = echo cat
+redir = < < > < > <
+file = blabla -R ls avion -c -e
+
+
+exec()
+{
+        OUVRIR FICHIER!!!!!!!!!!!!!!!!!!!
+        ca foire on quitte + message erreur
+        VERIFIER COMMAND
+        ca foire on quitte
+}
+
+ls < infile1 -R -l | < infile1 > outfile1 echo bonjour > outfile2 >> append1 | < Makefile
+
+ft_print_struct(){}
+COMMANDE 1:
+cmd = ls
+arg = ls -R -l
+type <
+file infile1
+COMMANDE 2:
+cmd = echo
+arg = echo bonjour
+type < > > >>
+file infile1 outfile1 outfile2 append1  
+COMMANDE 3:
+cmd = NULL
+arg = NULL
+type <
+file Makefile
