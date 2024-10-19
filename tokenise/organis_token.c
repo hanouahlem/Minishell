83950d3320caf52b91b2f52df419e41707d07d46
Ahlem Bey <ahbey@paul-f4Ar6s3.clusters.42paris.fr>
2024-10-19 19:43:39 +0200

    add fonction organis_tokenapres norme

--- tokenise/organis_token.c
+++ tokenise/organis_token.c
@@ -6,7 +6,7 @@
 /*   By: ahbey <ahbey@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 15:57:11 by ahbey             #+#    #+#             */
-/*   Updated: 2024/10/19 18:08:19 by ahbey            ###   ########.fr       */
+/*   Updated: 2024/10/19 19:24:25 by ahbey            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,8 @@ void	add_arg_to_cmd(t_parse *cmd, char *arg)
 	while (cmd->args && cmd->args[i])
 	{
 		new_args[i] = cmd->args[i];
-		printf("new_args == %s\n", new_args[i]);
+		// printf("new_args == %s\n", new_args[i]);
 		i++;
-		
 	}
 	new_args[i] = ft_strdup(arg);
 	new_args[i + 1] = NULL;
@@ -53,73 +52,133 @@ t_parse	*create_cmd(t_parse *new_cmd, t_mini *data)
 	new_cmd->cmd = NULL;
 	new_cmd->args = NULL;
 	// new_cmd->num_redir_in = 0;
-	// new_cmd->num_redir_out = 0; 
-	new_cmd->tok = data->token ;
+	// new_cmd->num_redir_out = 0;
+	new_cmd->tok = data->token;
 	new_cmd->next = NULL;
 	new_cmd->prev = NULL;
 	return (new_cmd);
 }
 
+// void	ft_cmd_organis(t_mini *data)
+// {
+// 	t_parse	*cmd;
+// 	int		skip_arg;
+
+// 	skip_arg = 0;
+// 	cmd = NULL;
+// 	// print_token(data->token);
+// 	while (data->token)
+// 	{
+// 		if (data->token->type != PIPE)
+// 		{
+// 			// gerer echo hello > f1 > f2 > f3 > f4 on doit ecrire que dans f4
+// 			if (data->token->type == WORD)
+// 			{
+// 				if (!skip_arg)
+// 				{
+// 					if (!cmd || (data->token->prev
+// 							&& data->token->prev->type == PIPE))
+// 					{
+// 						cmd = create_cmd(cmd, data);
+// 						cmd->cmd = ft_strdup(data->token->value_t);
+// 						printf("cmd = %s\n", cmd->cmd);
+// 					}
+// 					else
+// 					{
+// 						add_arg_to_cmd(cmd, data->token->value_t);
+// 						printf("arg = %s\n", data->token->value_t);
+// 					}
+// 				}
+// 				else
+// 					skip_arg = 0;
+// 			}
+// 			// Gérer les redirections
+// 			else if (data->token->type == REDIR_IN)
+// 			{
+// 				if (data->token->next && data->token->next->type == WORD)
+// 				{
+// 					data->token->type = IN_FILE;
+// 					// printf("data->token->type ====> [%d]\n",
+// 					// data->token->type);
+// 					// printf("data->token->val ====> [%s]\n",
+// 					// data->token->value_t);
+// 					skip_arg = 1;
+// 				}
+// 			}
+// 			else if (data->token->type == REDIR_OUT)
+// 			{
+// 				if (data->token->next && data->token->next->type == WORD)
+// 				{
+// 					data->token->type = OUT_FILE;
+// 					// printf("data->token->type ====> [%d]\n",
+// 					// data->token->type);
+// 					// printf("data->token->val ====> [%s]\n",
+// 					// data->token->value_t);
+// 					skip_arg = 1;
+// 				}
+// 			}
+// 		}
+// 		else if (data->token->type == PIPE)
+// 		{
+// 			printf("il y a un PIPE, on passe a la commande suivante\n");
+// 			cmd = NULL; // Réinitialiser pour la prochaine commande
+// 		}
+// 		data->token = data->token->next;
+// 	}
+// }
+
+void	ft_organis_redir(t_mini *data, int *skip_arg)
+{
+	if ((data->token->type == REDIR_IN || data->token->type == REDIR_OUT)
+		&& data->token->next && data->token->next->type == WORD)
+	{
+		if (data->token->type == REDIR_IN)
+			data->token->type = IN_FILE;
+		else if (data->token->type == REDIR_OUT)
+			data->token->type = OUT_FILE;
+		*skip_arg = 1;
+	}
+}
+
+void	ft_organis_cmd_arg(t_mini *data, t_parse **cmd, int *skip_arg)
+{
+	if (data->token->type == WORD && !(*skip_arg))
+	{
+		if (!(*cmd) || (data->token->prev && data->token->prev->type == PIPE))
+		{
+			*cmd = create_cmd(*cmd, data);
+			(*cmd)->cmd = ft_strdup(data->token->value_t);
+			printf("cmd = %s\n", (*cmd)->cmd);
+		}
+		else
+		{
+			add_arg_to_cmd(*cmd, data->token->value_t);
+			printf("arg = %s\n", data->token->value_t);
+		}
+	}
+	else
+		*skip_arg = 0;
+}
+
 void	ft_cmd_organis(t_mini *data)
 {
 	t_parse	*cmd;
-	int skip_arg = 0;
+	int		skip_arg;
 
 	cmd = NULL;
-	// print_token(data->token);
+	skip_arg = 0;
 	while (data->token)
 	{
 		if (data->token->type != PIPE)
 		{
-			// gerer echo hello > f1 > f2 > f3 > f4 on doit ecrire que dans f4
-			if (data->token->type == WORD)
-			{	
-				if (!skip_arg)
-				{
-					if (!cmd || (data->token->prev && data->token->prev->type == PIPE))
-					{
-						cmd = create_cmd(cmd, data);
-						cmd->cmd = ft_strdup(data->token->value_t);
-						printf("cmd = %s\n", cmd->cmd);
-					}
-					else
-					{
-						add_arg_to_cmd(cmd, data->token->value_t);
-						printf("arg = %s\n", data->token->value_t);
-					}
-				}
-				else
-					skip_arg = 0;
-			}
-			// Gérer les redirections
-			else if (data->token->type == REDIR_IN)
-			{
-				if (data->token->next && data->token->next->type == WORD)
-				{
-					data->token->type = IN_FILE;
-					
-					// printf("data->token->type ====> [%d]\n", data->token->type);
-					// printf("data->token->val ====> [%s]\n", data->token->value_t);
-					skip_arg = 1; 
-				}
-			}
-			else if (data->token->type == REDIR_OUT)
-			{
-				if (data->token->next && data->token->next->type == WORD)
-				{
-					data->token->type = OUT_FILE;
-					// printf("data->token->type ====> [%d]\n", data->token->type);
-					// printf("data->token->val ====> [%s]\n", data->token->value_t);
-					skip_arg = 1; 
-				}
-			}
+			ft_organis_cmd_arg(data, &cmd, &skip_arg);
+			ft_organis_redir(data, &skip_arg);
 		}
-		else if (data->token->type == PIPE)
+		else
 		{
+			cmd = NULL;
 			printf("il y a un PIPE, on passe a la commande suivante\n");
-			cmd = NULL; // Réinitialiser pour la prochaine commande
 		}
 		data->token = data->token->next;
-		
 	}
 }
